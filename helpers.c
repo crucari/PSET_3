4fff5d353ec4a43b2fa2d7b2c22c5cb0d91cdf19
Carina Cruz <lecruz@bu.edu>
2018-04-23 22:54:33 +0000

    helpers.c in progress

--- helpers.c
+++ helpers.c
@@ -5,10 +5,20 @@
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
+// Input as a string a fraction and convert it into some integral number of eighths.
+// You may assume that duration will only be passed a string formatted as X/Y, whereby each of X and Y is a positive decimal digit, and Y is, moreover, a power of 2.
 int duration(string fraction)
 {
-    // TODO
+// TODO
+// 1)Ask for string (fraction)
+// 2)Take the numerator
+// 3) Take the denomenator
+// 4) use a common denomenator
+// 5) Math to get the number of eights: (Common denominator divided by numberator) x numerator
+// 6) Print number of eighths
 }
+
+printf(%n, eights) //prints number of eigths
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
